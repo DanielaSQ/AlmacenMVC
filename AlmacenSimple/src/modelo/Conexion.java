4a13756fea240a3e8034974a37a15b5e028d45d6
Daniela Quidel Quidel <ninlil.dsq.01@gmail.com>
2017-07-14 22:38:34 -0400

    subida conexion

---
+++ AlmacenSimple/src/modelo/Conexion.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Daniela
+ */
+public class Conexion {
+    
+    private final String bd = "almacen";
+    private final String url = "jdbc:mysql://localhost/almacen";
+    private final String login = "root";
+    private final String password = "";
+    
+    public Connection connection;
+    
+    public Conexion() throws SQLException{
+        try{
+            connection = (Connection) DriverManager.getConnection(url, login, password);
+            if(connection != null){
+                System.err.println("conexion a la base de datos [" + bd + "] OK"); 
+            }
+        }catch (SQLException e) {
+            System.out.println(e);
+        }catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+    
+    public Connection getConnection(){
+        return connection;
+    }
+}
