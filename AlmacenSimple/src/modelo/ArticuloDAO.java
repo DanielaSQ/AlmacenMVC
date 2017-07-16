0b587dd9c0fdbce22e2c8919892b3599a11e8e4b
Daniela Quidel Quidel <ninlil.dsq.01@gmail.com>
2017-07-16 17:41:32 -0400

    metodos agregar, obtener en articulo

--- AlmacenSimple/src/modelo/ArticuloDAO.java
+++ AlmacenSimple/src/modelo/ArticuloDAO.java
@@ -5,7 +5,13 @@
  */
 package modelo;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -14,17 +20,49 @@ import java.util.ArrayList;
 public class ArticuloDAO {
     
     public boolean agregarArticulo(ArticuloVO artVO){
-        
-        
-        return true;
+        try {
+            Conexion conn = new Conexion();
+            String sentencia = "";
+            Statement estatuto = conn.getConnection().createStatement();
+            
+            sentencia += "INSERT INTO articulo VALUES ('"
+                    + artVO.getCodigo()+ "', '"
+                    + artVO.getDetalle()+ "', '"
+                    + artVO.getCategoria()+ "', '"
+                    + artVO.getCantidadExistente()+ "', '"
+                    + artVO.getPrecioVenta()+ "')";
+            
+            estatuto.execute(sentencia);
+            
+            return true; 
+        } catch (SQLException ex) {
+            Logger.getLogger(ArticuloDAO.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+       
     }
     
-    public ArticuloVO obtenerArticuloPorCodigo(ArticuloVO artVO){
-        int codigo = artVO.getCodigo();
-        ArticuloVO articulo = new ArticuloVO();
+    public ArticuloVO obtenerArticuloPorCodigo(int codigo){
+       ArticuloVO articuloVO = new ArticuloVO();
         
-         
-        return articulo;
+        try {
+            Conexion conn = new Conexion();
+            String sentencia = "SELECT * FROM articulo WHERE codigo=?";
+            PreparedStatement estatuto = conn.getConnection().prepareStatement(sentencia);
+            estatuto.setInt(1, codigo);
+            ResultSet resultado = estatuto.executeQuery();
+            
+            if (resultado.next()) {            
+                articuloVO.setCodigo(resultado.getInt("codigo"));
+                articuloVO.setDetalle(resultado.getString("detalle"));
+                articuloVO.setCategoria(resultado.getString("categoria"));
+                articuloVO.setCantidadExistente(resultado.getInt("cantidadExistente"));
+                articuloVO.setPrecioVenta(resultado.getInt("precioVenta"));
+            }            
+        } catch (SQLException ex) {
+            Logger.getLogger(ClienteDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return articuloVO;
     }
     
     public ArrayList<ArticuloVO> obtenerArticulos(){
@@ -40,7 +78,21 @@ public class ArticuloDAO {
         return articulo;
     }
     
-    public boolean borrarArticulo(ArticuloVO artVO){
+    public boolean eliminarPorCodigo(int codigo){
+        
+        try {
+            Conexion conn = new Conexion();
+            String sentencia = "DELETE FROM articulo WHERE codigo=?";
+            PreparedStatement estatuto = conn.getConnection().prepareStatement(sentencia);
+            estatuto.setInt(1,codigo);
+            int executeUpdateResult = estatuto.executeUpdate();
+            
+            if(executeUpdateResult == 0){
+                return false;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ClienteDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
         return true;
     }
 }
